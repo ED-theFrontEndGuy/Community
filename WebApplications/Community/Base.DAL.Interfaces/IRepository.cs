9b4afbe4d05e753953a5bdab1991bc2bf83f4afb
eriktaltech <erik.dolotov@taltech.ee>
2025-04-09 22:15:37 +0300

    Add first interface for DAL.

---
+++ WebApplications/Community/Base.DAL.Interfaces/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using Base.Domain;
+
+namespace Base.DAL.Interfaces;
+
+public interface IRepository<TEntity> where TEntity : BaseEntity
+{
+    IEnumerable<TEntity> All(Guid? userId = null);
+    Task<IEnumerable<TEntity>> AllAsync(Guid? userId = null);
+
+    TEntity Find(Guid id, Guid? userId = null);
+    Task<TEntity> FindAsync(Guid id, Guid? userId = null);
+
+    void Add(TEntity entity);
+
+    TEntity Update(TEntity entity);
+
+    void Remove(TEntity entity, Guid? userId = null);
+
+    void Remove(Guid id, Guid? userId = null);
+}
