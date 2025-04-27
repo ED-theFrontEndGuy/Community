d1312b0008d446bf6a740117c65d0e5160d44d88
eriktaltech <erik.dolotov@taltech.ee>
2025-04-27 20:06:26 +0300

    Prepare JS 02 to convert to TS

---
+++ Javascript/02/src/board.ts
@@ -0,0 +1,59 @@
+"use strict";
+
+import * as helpers from "./helpers.js";
+
+
+export function drawBoard(game) {
+    const board = document.getElementById("app");
+    const playerDiv = document.getElementById("current-player");
+    playerDiv.innerHTML = `Turn: ${game.currentPlayer.symbol}`;
+
+    for (let x = 0; x < 5; x++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+
+        for (let y=0; y < 5; y++) {
+            let cell = document.createElement("div");
+            cell.classList.add("cell");
+            cell.innerHTML = game.board[x][y];
+
+            if (game.board[x][y] !== null) {
+                cell.addEventListener("click", (e) => {
+                    let currentPlayer = game.currentPlayer;
+                    currentPlayer.makeAMove(game, x, y, e);
+                },
+                {once: true},
+                );
+            }
+
+            row.appendChild(cell);
+        }
+
+        board.appendChild(row);
+    }
+
+    helpers.drawPlayerPanels(game);
+
+    setListenersToActiveBoard(game, board);
+
+    return board;
+}
+
+
+function setListenersToActiveBoard(game, board) {
+    let [x, y] = game.activeBoardAnchor;
+    
+    for (let i = x; i < x+3; i++) {
+        let rowNode = board.childNodes[i];
+        
+        for (let j = y; j < y+3; j++) {
+            let cellNode = rowNode.childNodes[j];
+
+            cellNode.classList.add("active");
+            cellNode.addEventListener("click", (e) => {
+                let currentPlayer = game.currentPlayer;
+                currentPlayer.makeAMove(game, i, j, e);
+            });
+        }
+    }
+}
---
+++ Javascript/02/src/main.ts
@@ -0,0 +1,37 @@
+import './style.css'
+import * as helpers from "./helpers.js";
+import { GameBrain } from "./brain.js";
+import { drawBoard } from "./board.js";
+import { Player } from "./player.js";
+
+let playerX = new Player("X");
+let playerO = new Player("O");
+let game = new GameBrain(playerX, playerO);
+
+helpers.createBase();
+helpers.createGameResetButton(game, drawBoard);
+helpers.startTimer();
+
+let opponentButton = document.getElementById("opponent-btn");
+opponentButton.addEventListener("click", (e) => {
+    playerO.toggleAI();
+
+    let playerOtitle = document.getElementsByTagName("h1")[1];
+    console.log(playerOtitle);
+    
+    let moveButtons = document.getElementsByClassName("move-btn");
+
+    if (playerO.isAi) {
+        playerOtitle.innerHTML = `AI ${playerO.symbol}`;
+        Array.from(moveButtons).forEach(button => {
+            button.classList.add("disabled");
+        });
+    } else {
+        playerOtitle.innerHTML = `Player ${playerO.symbol}`
+        Array.from(moveButtons).forEach(button => {
+            button.classList.remove("disabled");
+        });
+    }
+})
+
+drawBoard(game);
