a4c912b3b4498846d61e0d093a713201a46cb678
eriktaltech <erik.dolotov@taltech.ee>
2025-05-28 22:59:20 +0300

    Set up for page.

---
+++ CFE/community-react/src/app/course/create/page.tsx
@@ -0,0 +1,5 @@
+export default function CourseCreate() {
+	return (
+		<> Create Course </>
+	);
+}
---
+++ CFE/community-react/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+	return (
+		<footer className="border-top footer text-muted">
+			<div className="container">
+				&copy; 2025 - Community
+			</div>
+		</footer>
+	);
+}
